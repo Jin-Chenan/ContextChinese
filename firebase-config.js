b44dd23996c26076c1ea9d0c2571ee10c332827e
Jin-Chenan <dominic.cammarata@outlook.com>
2026-08-29 18:33:32 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase project configuration.
+// Get these values from: Firebase Console > Project settings > General >
+// "Your apps" > SDK setup and configuration > Config.
+const firebaseConfig = {
+  apiKey: "REPLACE_WITH_YOUR_API_KEY",
+  authDomain: "REPLACE_WITH_YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
+  storageBucket: "REPLACE_WITH_YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "REPLACE_WITH_YOUR_SENDER_ID",
+  appId: "REPLACE_WITH_YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
